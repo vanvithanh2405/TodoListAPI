9035b60b204d562138659178edfe7ec9e67fc71b
Van Vi Thanh <van.thanh@aarista.com>
2025-09-29 17:22:01 +0700

    add config

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,6 +1,19 @@
+
+using Microsoft.EntityFrameworkCore;
+using TodoList.Infrastructure.Data.DbContexts;
+using TodoList.Infrastructure.Data.Mappers;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
+builder.Services.AddDbContext<MainDbContext>(options =>
+{
+    options.UseSqlServer(connectionString,
+                                providerOptions => { providerOptions.EnableRetryOnFailure(); providerOptions.UseCompatibilityLevel(120); });
+}, ServiceLifetime.Transient);
+
+builder.Services.AddAutoMapper(typeof(MainMapper));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
